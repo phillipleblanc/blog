6728a24a712759e43907435465becc68156a4436
Phillip LeBlanc <phillip.leblanc@microsoft.com>
2020-02-16 18:15:25 +0900

    More responsive!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,13 @@
 import React from "react"
 import Header from "../components/Header"
 import SEO from "../components/SEO"
+import "./index.css"
 
-const Layout = () => {
+const Index = () => {
   return (
     <>
     <SEO />
-    <main style={{
-      maxWidth: "1520px", 
-      marginTop: "40px",
-      marginRight: "auto", 
-      marginLeft: "auto",
-      padding:"0 200px 0 200px"
-      }}
-    >
+    <main>
       <Header />
       <div
         style={{
@@ -24,27 +18,19 @@ const Layout = () => {
         }}
       >
         <img src="profile_prof.jpg" width="400px" />
-        <h1 style={{fontSize: "65px", textAlign:"center"}}>Hi, I'm <strong>Phillip LeBlanc</strong>.<br /> A software engineer.</h1>
-        <hr style={{height: "3px", width: "100%", background: "rgba(0,0,0,0.3)", border: "none"}} />
-        <h2>Quick Bio</h2>
-        <div style={{fontSize: "20px", textAlign:"center", maxWidth:"500px"}}>
-          <p>
-          안녕하세요!
-          </p>
-          <p>
-          Living in Seoul, Korea leading a team of engineers to help make app developers more productive with Visual Studio App Center.
-          </p>
-          <p>
-          I moved to Seattle after accepting a software development position at Microsoft and graduating from Louisiana State University.
-          </p>
-          <p>
-          I worked for 5 years in Seattle on Azure Active Directory, and moved to Korea in 2017.
-          </p>
-        </div>
+        <h1>Hi, I'm <strong>Phillip LeBlanc</strong>.<br /> A software engineer.</h1>
+        <hr />
+        <h2>👋 About Me</h2>
+        <content>
+          <p>I lead a high performing team of software engineers at Microsoft.</p>
+          <p>Living in Seoul, Korea 🇰🇷.</p>
+          <p>Graduated from Louisiana State University.</p>
+          <p>Worked for 5 years in Seattle on Azure Active Directory.</p>
+        </content>
       </div>
     </main>
     </>
   )
 }
 
-export default Layout
+export default Index

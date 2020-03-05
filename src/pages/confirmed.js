f6bd920a5c31f8f35f27ac8f5ae1e5481022353a
Phillip LeBlanc <phillip.leblanc@outlook.com>
2020-03-05 21:00:06 +0900

    Adding thankyou gif

--- src/pages/confirmed.js
+++ src/pages/confirmed.js
@@ -7,12 +7,22 @@ const NewsletterSignUpConfirmed = () => {
     <>
       <Header />
       <main>
-        <h1><span role="img" aria-label="Tada Emoji">🎉</span> You're subscribed!</h1>
+        <h1>
+          <span role="img" aria-label="Tada Emoji">
+            🎉
+          </span>{" "}
+          You're subscribed!
+        </h1>
         <content>
-        <p>
-            Thanks for signing up for my newsletter!
-            You will now receive all of my future articles and insights about the world.
-        </p>
+          <p>
+            Thanks for signing up for my newsletter! You will now receive all of
+            my future articles and insights about the world.
+          </p>
+          <p>
+            Look now for a welcome email from me! Check your Junk folder if you
+            don't see it.
+          </p>
+          <img alt="Dog smiling with the text Thank You" src="/thankyou.gif" />
         </content>
       </main>
     </>
